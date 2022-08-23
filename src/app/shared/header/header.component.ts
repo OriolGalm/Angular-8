dc22361c4b614d5da584533ef475ce781b62a30f
Oriol <oriol008@gmail.com>
2022-08-23 13:25:34 +0200

    exercici 6.4

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -14,6 +14,49 @@ export class HeaderComponent implements OnInit {
   constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.initLogin();
+    this.signupForm = this.initForm();
+    this.getLocalStorage();
+    //localStorage.setItem("signupForm", JSON.stringify( this.signupForm ))
   }
 
+  getLocalStorage(){
+    let person = localStorage.getItem(JSON.parse("userData"));
+    console.log(person)
+  }
+
+  onSubmit(response:any){
+    localStorage.setItem("userData", JSON.stringify( response ))
+    
+  }
+
+  onLogin(response:any){
+    /* let email = response.emailLogin;
+    let password = response.passLogin;
+    let userDataEmail = localStorage.getItem(JSON.parse( 'userData.email' ));
+    let userDataPass = localStorage.getItem(JSON.parse( 'userData.password' ));
+    let userDataFirstNanme = localStorage.getItem(JSON.parse( 'userData.firstName' ));
+    let userDataLastName = localStorage.getItem(JSON.parse( 'userData.lastName' ));
+    if(email == userDataEmail && password == userDataPass){
+      alert("Hello " + userDataFirstNanme + userDataLastName);
+    } */
+  }
+
+  initLogin():FormGroup{
+    return this.fb.group({
+      emailLogin:['', Validators.required],
+      passLogin:['', Validators.required]
+    })
+  }
+
+  initForm():FormGroup{
+    return this.fb.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      email:['',[ Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  
 }

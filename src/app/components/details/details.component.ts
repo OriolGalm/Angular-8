4384e69cc453c2c0eb9b50dac8bb59de4d2d240b
OriolGalm <accions018@gmail.com>
2022-09-12 13:02:35 +0200

    exercici 8.1

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SwarsService } from 'src/app/swars.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -14,7 +15,7 @@ export class DetailsComponent implements OnInit {
   public nau$!: Observable<any>;
   public imgNau!: any;
   public strs!:any;
-  public pilots:any = [];
+  public pilots!:any;
 
   constructor(private readonly route: ActivatedRoute, 
     private readonly swSvc: SwarsService,
@@ -24,9 +25,20 @@ export class DetailsComponent implements OnInit {
     const urlNau = this.route.snapshot.params['url'];
     this.nau$ = this.http.get(urlNau);
     this.strs = urlNau.split('/')[5];
-    /* this.pilots = urlNau+"pilots";
-    console.log("Pilots: ", this.pilots); */
+    //this.pilots = this.http.get("https://swapi.dev/api/starships/12")
+    //this.pilots = urlNau+"pilots";
+    //console.log("Pilots: ", this.pilots);   
+    this.getPilots().subscribe(data => {this.pilots = data;
+    this.pilots = Array.of(this.pilots);
+    console.log("Pilots: ", this.pilots);
+    })
   }
 
+  getPilots(){
+    return this.http.get("https://swapi.dev/api/starships/12")
+    .pipe(
+    map(res => this.pilots = res)
+    )
+  }
 
 }
